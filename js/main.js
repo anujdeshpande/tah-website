990923a1c03355ec75f560b749fda07cb3d6d750
Siddharth Bhagwan <sid_sss@hotmail.com>
2015-02-19 14:58:47 +0530

    Social links, font and skrollr fixes

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   var frame_sections = [0, 1950, 8550, 12400, 18900, 30200, 32700];
   var frame_durations = [3000, 5000, 5000, 8000, 16000, 3000];
   var current_frame = frame_sections[i];
-  console.log(current_frame);
+  // console.log(current_frame);
 
   function call_next_frame() {
     var next_frame;
@@ -14,7 +14,7 @@ $(document).ready(function() {
       return next_frame;
     } else { 
       next_frame = frame_sections[++i];
-      console.log('Next frame is ' + next_frame);
+      // console.log('Next frame is ' + next_frame);
       return next_frame;
     }
   }
@@ -23,64 +23,63 @@ $(document).ready(function() {
     var prev_frame;
     if (i == 0) {
       prev_frame = frame_sections[i];
-      console.log('Prev frame is ' + prev_frame);
+      // console.log('Prev frame is ' + prev_frame);
       return prev_frame;
     } else { 
       prev_frame = frame_sections[--i];
-      console.log('Prev frame is ' + prev_frame);
+      // console.log('Prev frame is ' + prev_frame);
       return prev_frame;
     }
   }
 
-  // $("html, body").bind({'mousewheel DOMMouseScroll onmousewheel touchmove scroll': 
-  //   function(e) {
-  //     if (e.target.id == 'el') return;
-  //     e.preventDefault();
-  //     e.stopPropagation();
+  $("html, body").bind({'mousewheel DOMMouseScroll onmousewheel touchmove scroll': 
+    function(e) {
+      if (e.target.id == 'el') return;
+      e.preventDefault();
+      e.stopPropagation();
 
-  //     //Determine Direction
-  //     if (e.originalEvent.wheelDelta && e.originalEvent.wheelDelta >= 0) {
-  //       //Up
-  //       if (!$('*').is(':animated')) {
-  //         $('body').stop().scrollTo(call_prev_frame(), { duration: frame_durations[i], easing : 'swing' });
-  //         console.log(' Up i = ' + i + ' ' + frame_durations[i]);
-  //       } else {
-  //         console.log ('Up Scroll Stopped');
-  //         return;
-  //       }
-
-  //     } else if (e.originalEvent.detail && e.originalEvent.detail <= 0) {
-  //       //Up
-  //       if (!$('*').is(':animated')) {
-  //         $('body').stop().scrollTo(call_prev_frame(), { duration: frame_durations[i], easing : 'swing' });
-  //         console.log(' Up i = ' + i + ' ' + frame_durations[i]);
-  //       } else {
-  //         console.log ('Up Scroll Stopped 2');
-  //         return;
-  //       }
-
-  //     } else {
-  //       //Down
-  //       if (!$('*').is(':animated')) {
-  //         $('body').stop().scrollTo(call_next_frame(), { duration: frame_durations[i-1], easing : 'swing' });
-  //         console.log('Down i = ' + (i-1) + ' ' + frame_durations[i -1]);
-  //       } else {
-  //         console.log ('Down Scroll Stopped');
-  //         return;
-  //       }
-  //     }
-  //   }
-  // });
-});
-$(document).ready(function(){
-    $("#nav-mobile").html($("#nav-main").html());
-    $("#nav-trigger span").click(function(){
-        if ($("nav#nav-mobile ul").hasClass("expanded")) {
-            $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
-            $(this).removeClass("open");
+      //Determine Direction
+      if (e.originalEvent.wheelDelta && e.originalEvent.wheelDelta >= 0) {
+        //Up
+        if (!$('*').is(':animated')) {
+          $('body').stop().scrollTo(call_prev_frame(), { duration: frame_durations[i], easing : 'swing' });
+          // console.log(' Up i = ' + i + ' ' + frame_durations[i]);
         } else {
-            $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
-            $(this).addClass("open");
+          // console.log ('Up Scroll Stopped');
+          return;
         }
-    });
+
+      } else if (e.originalEvent.detail && e.originalEvent.detail <= 0) {
+        //Up
+        if (!$('*').is(':animated')) {
+          $('body').stop().scrollTo(call_prev_frame(), { duration: frame_durations[i], easing : 'swing' });
+          // console.log(' Up i = ' + i + ' ' + frame_durations[i]);
+        } else {
+          // console.log ('Up Scroll Stopped 2');
+          return;
+        }
+
+      } else {
+        //Down
+        if (!$('*').is(':animated')) {
+          $('body').stop().scrollTo(call_next_frame(), { duration: frame_durations[i-1], easing : 'swing' });
+          // console.log('Down i = ' + (i-1) + ' ' + frame_durations[i -1]);
+        } else {
+          // console.log ('Down Scroll Stopped');
+          return;
+        }
+      }
+    }
+  });
+
+  $("#nav-mobile").html($("#nav-main").html());
+  $("#nav-trigger span").click(function(){
+    if ($("nav#nav-mobile ul").hasClass("expanded")) {
+      $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+      $(this).removeClass("open");
+    } else {
+      $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+      $(this).addClass("open");
+    }
+  });
 });
