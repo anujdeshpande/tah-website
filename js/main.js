12651ad6eb856d3f6c0f612cc21c3b4f3c947fa8
Siddharth Bhagwan <sid_sss@hotmail.com>
2015-02-21 18:22:33 +0530

    Responsivity bugs fixed. Start page formatted

--- js/main.js
+++ js/main.js
@@ -1,76 +1,76 @@
 $(document).ready(function() {
   if (!navigator.userAgent.match(/Mobi/)) {
-    var i = 0;
-    var frame_sections = [0, 1950, 8550, 12400, 18900, 30200, 32700];
-    var frame_durations = [3000, 5000, 5000, 8000, 16000, 3000];
-    var current_frame = frame_sections[i];
-    // console.log(current_frame);
+    // var i = 0;
+    // var frame_sections = [0, 1950, 8550, 12400, 18900, 30200, 32700];
+    // var frame_durations = [3000, 5000, 5000, 8000, 16000, 3000];
+    // var current_frame = frame_sections[i];
+    // // console.log(current_frame);
 
-    function call_next_frame() {
-      var next_frame;
-      if (i == frame_sections.length - 1) {
-        next_frame = frame_sections[i];
-        // console.log('Next frame is ' + next_frame);
-        return next_frame;
-      } else { 
-        next_frame = frame_sections[++i];
-        // console.log('Next frame is ' + next_frame);
-        return next_frame;
-      }
-    }
+    // function call_next_frame() {
+    //   var next_frame;
+    //   if (i == frame_sections.length - 1) {
+    //     next_frame = frame_sections[i];
+    //     // console.log('Next frame is ' + next_frame);
+    //     return next_frame;
+    //   } else { 
+    //     next_frame = frame_sections[++i];
+    //     // console.log('Next frame is ' + next_frame);
+    //     return next_frame;
+    //   }
+    // }
 
-    function call_prev_frame() {
-      var prev_frame;
-      if (i == 0) {
-        prev_frame = frame_sections[i];
-        // console.log('Prev frame is ' + prev_frame);
-        return prev_frame;
-      } else { 
-        prev_frame = frame_sections[--i];
-        // console.log('Prev frame is ' + prev_frame);
-        return prev_frame;
-      }
-    }
+    // function call_prev_frame() {
+    //   var prev_frame;
+    //   if (i == 0) {
+    //     prev_frame = frame_sections[i];
+    //     // console.log('Prev frame is ' + prev_frame);
+    //     return prev_frame;
+    //   } else { 
+    //     prev_frame = frame_sections[--i];
+    //     // console.log('Prev frame is ' + prev_frame);
+    //     return prev_frame;
+    //   }
+    // }
 
-    $('html, body').bind({'mousewheel DOMMouseScroll onmousewheel touchmove scroll': 
-      function(e) {
-        if (e.target.id == 'el') return;
-        e.preventDefault();
-        e.stopPropagation();
+    // $('html, body').bind({'mousewheel DOMMouseScroll onmousewheel touchmove scroll': 
+    //   function(e) {
+    //     if (e.target.id == 'el') return;
+    //     e.preventDefault();
+    //     e.stopPropagation();
 
-        //Determine Direction
-        if (e.originalEvent.wheelDelta && e.originalEvent.wheelDelta >= 0) {
-          //Up
-          if (!$('*').is(':animated')) {
-            $('body').stop().scrollTo(call_prev_frame(), { duration: frame_durations[i], easing : 'swing' });
-            // console.log(' Up i = ' + i + ' ' + frame_durations[i]);
-          } else {
-            // console.log ('Up Scroll Stopped');
-            return;
-          }
+    //     //Determine Direction
+    //     if (e.originalEvent.wheelDelta && e.originalEvent.wheelDelta >= 0) {
+    //       //Up
+    //       if (!$('*').is(':animated')) {
+    //         $('body').stop().scrollTo(call_prev_frame(), { duration: frame_durations[i], easing : 'swing' });
+    //         // console.log(' Up i = ' + i + ' ' + frame_durations[i]);
+    //       } else {
+    //         // console.log ('Up Scroll Stopped');
+    //         return;
+    //       }
 
-        } else if (e.originalEvent.detail && e.originalEvent.detail <= 0) {
-          //Up
-          if (!$('*').is(':animated')) {
-            $('body').stop().scrollTo(call_prev_frame(), { duration: frame_durations[i], easing : 'swing' });
-            // console.log(' Up i = ' + i + ' ' + frame_durations[i]);
-          } else {
-            // console.log ('Up Scroll Stopped 2');
-            return;
-          }
+    //     } else if (e.originalEvent.detail && e.originalEvent.detail <= 0) {
+    //       //Up
+    //       if (!$('*').is(':animated')) {
+    //         $('body').stop().scrollTo(call_prev_frame(), { duration: frame_durations[i], easing : 'swing' });
+    //         // console.log(' Up i = ' + i + ' ' + frame_durations[i]);
+    //       } else {
+    //         // console.log ('Up Scroll Stopped 2');
+    //         return;
+    //       }
 
-        } else {
-          //Down
-          if (!$('*').is(':animated')) {
-            $('body').stop().scrollTo(call_next_frame(), { duration: frame_durations[i-1], easing : 'swing' });
-            // console.log('Down i = ' + (i-1) + ' ' + frame_durations[i -1]);
-          } else {
-            // console.log ('Down Scroll Stopped');
-            return;
-          }
-        }
-      }
-    });
+    //     } else {
+    //       //Down
+    //       if (!$('*').is(':animated')) {
+    //         $('body').stop().scrollTo(call_next_frame(), { duration: frame_durations[i-1], easing : 'swing' });
+    //         // console.log('Down i = ' + (i-1) + ' ' + frame_durations[i -1]);
+    //       } else {
+    //         // console.log ('Down Scroll Stopped');
+    //         return;
+    //       }
+    //     }
+    //   }
+    // });
   }
 
   $('#nav-mobile').html($('#nav-main').html());
